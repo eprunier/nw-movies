0eb7a1229ee905e6f5ae1ae343295a87ec50c7e5
Eric Prunier <eric.prunier@gmail.com>
2014-12-01 22:34:07 +0100

    Refactoring of app initialization

--- app/app.js
+++ app/app.js
@@ -1,27 +1,47 @@
-movies = angular.module('movies', ['ngResource', 'ngRoute']);
+var config = configure();
+init(config);
 
-/* Router */
-movies.config(function ($routeProvider) {
-    $routeProvider
-    .when('/', {
-        templateUrl: 'app/views/home.html',
-        controller: 'homeController'
-    }).when('/movie/:id', {
-        templateUrl: 'app/views/movie.html',
-        controller: 'movieController'
-    }).when('/add', {
-        templateUrl: 'app/views/addMovie.html',
-        controller: 'addMovieController'
-    }).otherwise({
-        redirectTo: '/'
+function configure() {
+    var fs = require('fs');
+    var userHome = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
+    var moviesHome = userHome + '/.nw-movies';
+    fs.mkdir(moviesHome, function () {});
+
+    return {
+        home: moviesHome
+    };
+}
+
+function init(config) {
+    movies = angular.module('movies', ['ngResource', 'ngRoute']);
+
+    /* Controllers */
+    movies.controller('mainController', require('./app/controllers/mainController'));
+    movies.controller('homeController', require('./app/controllers/homeController'));
+    movies.controller('movieController', require('./app/controllers/movieController'));
+    movies.controller('addMovieController', require('./app/controllers/addMovieController'));
+
+    /* Routes */
+    movies.config(function ($routeProvider) {
+        $routeProvider
+        .when('/', {
+            templateUrl: 'app/views/home.html',
+            controller: 'homeController'
+        }).when('/movie/:id', {
+            templateUrl: 'app/views/movie.html',
+            controller: 'movieController'
+        }).when('/add', {
+            templateUrl: 'app/views/addMovie.html',
+            controller: 'addMovieController'
+        }).otherwise({
+            redirectTo: '/'
+        });
     });
-});
 
-/* Controllers */
-movies.controller('mainController', require('./app/controllers/mainController'));
-movies.controller('homeController', require('./app/controllers/homeController'));
-movies.controller('movieController', require('./app/controllers/movieController'));
-movies.controller('addMovieController', require('./app/controllers/addMovieController'));
+    /* Services */
+    movies.provider('db', require('./app/services/db'));
 
-/* Services */
-movies.factory('db', require('./app/services/db'));
+    movies.config(['dbProvider', function (dbProvider) {
+        dbProvider.setHome(config.home);
+    }]);
+}
